b502050761dda22370aed74320459595751922f9
Luis Carlos Del Piero <lchb1303@hotmail.com>
2017-08-03 14:23:02 -0500

    Devuelve la esperanza (o media) de las distribuciones discretas mas conocidas

---
+++ Media.R
@@ -0,0 +1,27 @@
+mbernoulli <- function(p){
+  return(p)
+}
+
+mbin <- function(n,p){
+  return(n*p)
+}
+
+mgeom <- function(p){
+  return(1/p)
+}
+
+mbin.n <- function(r,p){
+  return(r/p)
+}
+
+mhgeom <- function(r,n,N){
+  return(r*(n/N))
+}
+
+mpoisson <- function(l){
+  return(l)
+  
+  prueba <- function(i){
+    if(i == "mbernoulli"){
+      
+    }
